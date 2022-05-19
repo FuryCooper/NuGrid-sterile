7507abc52a952eb18973df29aebd5eb636f12c69
RichardFury <1397320399@qq.com>
2022-05-19 09:28:25 +0800

    clean

--- de2_deduction/Gadget-2.0.7/Gadget2/twotypes-deductcdm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mpirun -np 8 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m000-xi000-lcdmgas-twotypes.param49
--- de2_deduction/Gadget-2.0.7/Gadget2/twotypes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mpirun -np 16 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m020-xi000-lcdmgas-twotypes.param49
-mpirun -np 16 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m040-xi000-lcdmgas-twotypes.param49
-mpirun -np 16 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m060-xi000-lcdmgas-twotypes.param49
-mpirun -np 16 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m080-xi000-lcdmgas-twotypes.param49
-mpirun -np 16 ./Gadget2 ./rui/Rui_twotypes/Rui_cmbonly-m100-xi000-lcdmgas-twotypes.param49
-
